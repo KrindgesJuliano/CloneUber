d7f39c91dca5cd6fd56c05d4e5a9041a11fb5441
JulianoKrindges <juliano9krindges@hotmail.com>
2020-01-03 17:02:39 -0300

    add mapview and get GPS location

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-
-import {View} from 'react-native';
+import Map from './components/Map';
 
 // import { Container } from './styles';
 
-const App = () => (
-  <View style={{flex: 1}}>
-    <MapView
-      style={{flex: 1}}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      showsUserLocation
-      loadingEnabled
-    />
-  </View>
-);
+const App = () => <Map />;
 
 export default App;
